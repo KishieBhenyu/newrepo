ff8bb5ce4ce951b4f1ebe4cc35e698da150fab87
kishie <pharrelz62@gmail.com>
2026-03-07 10:41:25 +0200

    first

--- server.js
+++ server.js
@@ -6,6 +6,7 @@
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
@@ -27,4 +28,9 @@ const host = process.env.HOST
  *************************/
 app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`)
+})
+
+//index route
+app.get("/", function(req, res) {
+  res.render("index", { title: "Home" })
 })
